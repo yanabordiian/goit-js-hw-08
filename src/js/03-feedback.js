f7c29b59b0d18b8bf4cdceb732092e93295529b6
yanabordiian <98254244+yanabordiian@users.noreply.github.com>
2023-08-31 14:45:07 +0200

    d

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('[name="email"]'),
+    textarea: document.querySelector('[name="message"]'),
+
+}
+//refs.form.addEventListener('submit');
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onTextareaInput(e){
+    const message = e.target.value;
+ 
+    localStorage.setItem('feedback-form-state', message);
+}
