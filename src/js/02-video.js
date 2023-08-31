d5e6849a4324b2589ed367b2b112cddc1a92e3d8
yanabordiian <98254244+yanabordiian@users.noreply.github.com>
2023-08-31 13:47:43 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = ' videoplayer-current-time';
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
+
+const updateCurrentTime = data => {
+    localStorage.setItem(CURRENT_TIME, data.seconds);
+};
+player.on('timeupdate', throttle(updateCurrentTime, 1000));
