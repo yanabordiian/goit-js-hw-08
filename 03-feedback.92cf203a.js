36f8d10bc45cdb2640fb1e8c08ab7c01ae0c88ac
yanabordiian <98254244+yanabordiian@users.noreply.github.com>
2023-08-31 12:45:34 +0000

    Deploying to gh-pages from @ yanabordiian/goit-js-hw-08@f7c29b59b0d18b8bf4cdceb732092e93295529b6 🚀

---
+++ 03-feedback.92cf203a.js
@@ -0,0 +1,2 @@
+({form:document.querySelector(".feedback-form"),email:document.querySelector('[name="email"]'),textarea:document.querySelector('[name="message"]')}).textarea.addEventListener("input",(function(e){const t=e.target.value;localStorage.setItem("feedback-form-state",t)}));
+//# sourceMappingURL=03-feedback.92cf203a.js.map
---
+++ 03-feedback.d7c82166.js
@@ -0,0 +1,2 @@
+(document.querySelector(".feedback-form"),document.querySelector('[name="email"]'),document.querySelector('[name="message"]')).addEventListener("input",(function(e){var t=e.target.value;localStorage.setItem("feedback-form-state",t)}));
+//# sourceMappingURL=03-feedback.d7c82166.js.map
